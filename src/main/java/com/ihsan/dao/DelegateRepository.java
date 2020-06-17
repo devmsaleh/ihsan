724fcabac301c8964b43f62502aaa53bcbad605e
devmsaleh <dev.mahmoud.saleh@gmail.com>
2020-06-17 14:07:48 +0300

    fix

--- src/main/java/com/ihsan/dao/DelegateRepository.java
+++ src/main/java/com/ihsan/dao/DelegateRepository.java
@@ -15,7 +15,7 @@ public interface DelegateRepository extends JpaRepository<Delegate, BigInteger> 
 	@Query("from Delegate where lower(userName) = lower(:userName) and password =:password")
 	Delegate authenticate(@Param("userName") String userName, @Param("password") String password);
 
-	@Query(value = "select distinct CITY_ID from TM_DELEGATE_PRIVILEGES where DELEGATE_ID=:delegateId", nativeQuery = true)
+	@Query(value = "select distinct CITY_ID from TM_DELEGATE_PRIVILEGES where CITY_ID is not null and DELEGATE_ID=:delegateId", nativeQuery = true)
 	List<BigDecimal> getAutorizedEmaratesList(@Param("delegateId") BigInteger delegateId);
 
 	@Query(value = "select PROJECTNAME from POSPERMISSIONS where DELEGATEID=:delegateId and STATUS=0", nativeQuery = true)
